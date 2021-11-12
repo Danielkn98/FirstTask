ff890db50b7faa80a41ac458a9dae094de7f8c80
Daniil Korsun <danielkn98additional@gmail.com>
2021-11-12 16:13:52 +0300

    Изменение параметров поиска

--- src/test/java/FirstTask.java
+++ src/test/java/FirstTask.java
@@ -15,15 +15,15 @@ public class FirstTask{
     @Before
     public void start(){
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
+        wait = new WebDriverWait(driver, 15);
     }
 
     @Test
     public void FirstTask(){
-        driver.get("https://www.google.com");
+        driver.get("https://www.bing.com/");
         driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.id("btnG")).click();
-        wait.until(titleIs("webdriver - Поиск в Google"));
+        driver.findElement(By.id("search_icon")).click();
+        wait.until(titleIs("webdriver - Поиск"));
     }
 
     @After
