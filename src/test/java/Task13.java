af863460f4145fa58834c3f0d8dcf30c089c7e83
Daniil Korsun <danielkn98additional@gmail.com>
2021-12-02 17:57:31 +0300

    New tasks

---
+++ src/test/java/Task13.java
@@ -0,0 +1,80 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+
+public class Task13 {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        }
+        catch (NoSuchElementException ex){
+            return false;
+        }
+    }
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void task13(){
+        driver.get("http://localhost/litecart");
+        int numberOfProducts = 3;
+        for (int a = 0; a<numberOfProducts; a++) {
+            List<WebElement> productsMP = driver.findElements(By.cssSelector("#box-most-popular li"));
+            productsMP.get(0).click();
+            WebElement quantityInCart = driver.findElement(By.cssSelector("#cart .quantity"));
+            String quantity = quantityInCart.getAttribute("textContent");
+            int intQ = Integer.valueOf(quantity);
+            if(isElementPresent(By.cssSelector("[name='options[Size]']"))){
+                driver.findElement(By.cssSelector("[name='options[Size]']")).click();
+                driver.findElement(By.cssSelector("[name='options[Size]'] [value=Medium]")).click();
+            }
+            driver.findElement(By.cssSelector("button[value='Add To Cart']")).click();
+            wait.until(ExpectedConditions.attributeContains(quantityInCart, "textContent", Integer.toString(intQ + 1)));
+            if(a==numberOfProducts-1){
+                driver.findElement(By.cssSelector("a.link[href*=checkout]")).click();
+            }else{
+                driver.navigate().back();
+            }
+        }
+        wait.until(titleIs("Checkout | My Store"));
+        WebElement CheckoutSummary = driver.findElement(By.cssSelector("#box-checkout-summary"));
+        List <WebElement> CheckoutSummaryProducts = CheckoutSummary.findElements(By.cssSelector("td.item"));
+        int typesOfProductsInCart = CheckoutSummaryProducts.size();
+        for (int a = typesOfProductsInCart-1; a >=0; a--){
+            // Кликаем по первой попавшейся кнопке, т.к. порядок удаления нам не важен
+            driver.findElement(By.cssSelector("button[value=Remove]")).click();
+            if(a==typesOfProductsInCart-1){
+                wait.until(ExpectedConditions.stalenessOf(CheckoutSummary));
+            }else{
+                wait.until(ExpectedConditions.stalenessOf(CheckoutSummaryProducts.get(a)));
+            }
+        }
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
